85a917e83958d8c893156814e2a6bf1f2fdad29e
devo1929 <devo1929@gmail.com>
2022-12-17 20:30:24 -0500

    Move parsing of UPDATE and GAME messages to Channel class

---
+++ DXMainClient/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rampastring.XNAUI.XNAControls;
+
+namespace DTAClient.Extensions;
+
+public static class ServiceProviderExtensions
+{
+    public static T GetControl<T>(this ServiceProvider serviceProvider) where T : XNAControl
+        => serviceProvider.GetService<T>();
+}
